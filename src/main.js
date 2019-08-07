43cb86da438f20bc691bb08da396ecb5ef0f05bb
Gaute Meek Olsen <gaute.meek@gmail.com>
2019-08-07 19:47:50 +0200

    added firebase performance

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './registerServiceWorker'
+import firebaseinit from '@/firebaseinit.js'
+
+firebaseinit()
 
 Vue.config.productionTip = false
 
