606d8a6a2f1991493bdcedc71daf4d20a3b54c6b
Gaute Meek Olsen <gaute.meek@gmail.com>
2019-08-08 13:38:34 +0200

    TIL css empty

---
+++ src/views/til/2019-08-08CSSEmpty.vue
@@ -0,0 +1,27 @@
+
+<template>
+  <article>
+    <h1>Style elements without content</h1>
+    <blockquote class="twitter-tweet">
+      <p lang="en" dir="ltr">
+        <a href="https://twitter.com/hashtag/CSS?src=hash&amp;ref_src=twsrc%5Etfw">#CSS</a> :empty Selector ∅
+        <br />
+        <br />Often, we want to style elements that contain content. How about when an element has no children or text at all? Easy, you can use the :empty selector 🤩
+        <a
+          href="https://twitter.com/hashtag/100DaysOfCode?src=hash&amp;ref_src=twsrc%5Etfw"
+        >#100DaysOfCode</a>
+        <a
+          href="https://twitter.com/hashtag/CodeNewbie?src=hash&amp;ref_src=twsrc%5Etfw"
+        >#CodeNewbie</a>
+        <a
+          href="https://twitter.com/hashtag/301DaysOfCode?src=hash&amp;ref_src=twsrc%5Etfw"
+        >#301DaysOfCode</a>
+        <a href="https://t.co/5FVitKvPtw">pic.twitter.com/5FVitKvPtw</a>
+      </p>&mdash; Samantha Ming (@samantha_ming)
+      <a
+        href="https://twitter.com/samantha_ming/status/1157725595926581248?ref_src=twsrc%5Etfw"
+      >August 3, 2019</a>
+    </blockquote>
+    <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+  </article>
+</template>
