3ba1e227316c5600170e8817169dab2ed2dfd407
Gaute Meek Olsen <gaute.meek@gmail.com>
2019-08-12 20:27:52 +0200

    note added to til bitmap efficient

--- src/views/til/2019-08-12AndroidBitmapEfficient.vue
+++ src/views/til/2019-08-12AndroidBitmapEfficient.vue
@@ -11,5 +11,16 @@
         rel="noopener"
       >Glide</a> wich does the work for you.
     </p>
+    <h6>
+      Might change my projects to use
+      <a href="https://github.com/coil-kt/coil/" target="_blank" rel="noopener">Coil</a> instead in the future.
+      It looks promising, but was a little buggy at the moment. (12.08.2019)
+    </h6>
   </article>
 </template>
+
+<style scoped>
+h6 {
+  font-weight: 100;
+}
+</style>
