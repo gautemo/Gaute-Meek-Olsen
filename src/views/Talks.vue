142fbc5b19457fed3c386c6a54413c2d7fe8e4c5
Gaute Meek Olsen <gaute.meek@gmail.com>
2019-11-24 17:06:47 +0100

    added sentence about talk

--- src/views/Talks.vue
+++ src/views/Talks.vue
@@ -32,7 +32,10 @@ export default {
           text:
 `The definition of Firebase is "Firebase is Google’s mobile application development platform that helps you build, improve, and grow your app."
 But what does this mean? Firebase is multiple products and services hosted in the cloud and does that we don't have to write backend code.
-We will take a look at how Firebase offers database, authentication, analytics, file storage, push notifications, machine learning and a lot more directly on iOS, Android and web applications.`,
+We will take a look at how Firebase offers database, authentication, analytics, file storage, push notifications, machine learning and a lot more directly on iOS, Android and web applications.
+
+Every Firebase project is also a Google Cloud Platform project. I'll explain the relation and how they overlap.
+`,
           github: 'https://github.com/gautemo/fama',
           tech: ['Firebase', 'Web', 'Android', 'iOS'],
           color: '#FFA000'
