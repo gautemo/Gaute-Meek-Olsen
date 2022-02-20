f19b115fbbc6f7bf15b73d561346f071ecbce7ff
Gaute Meek Olsen <gaute.meek@gmail.com>
2022-02-20 09:48:24 +0100

    til icons

--- src/today-i-learned/components/TodayILearnedList.vue
+++ src/today-i-learned/components/TodayILearnedList.vue
@@ -1,7 +1,15 @@
 <script setup lang="ts">
 import { getUrl } from '../../utils/blogUtils'
 import { computed } from 'vue'
+import IconCss from '../assets/IconCss.vue'
+import IconGit from '../assets/IconGit.vue'
 const tilFiles = import.meta.globEager('../*.md')
+
+const icons = {
+  CSS: IconCss,
+  git: IconGit,
+}
+const getIcon = (name: string) => icons[name as keyof typeof icons]
 
 const categories = computed<{ [key: string]: { title: string; url: string }[] }>(() => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -20,10 +28,10 @@ const categories = computed<{ [key: string]: { title: string; url: string }[] }>
 <template>
   <section>
     <div v-for="[category, list] in Object.entries(categories)" :key="category">
-      <header>
-        <!-- <div class="icon" v-html="b.icon"></div> -->
-        <h2>{{ category }}</h2>
-      </header>
+      <h2>
+        <component :is="getIcon(category)" class="icon"></component>
+        <span>{{ category }}</span>
+      </h2>
       <ul>
         <li v-for="til in list" :key="til.url">
           <a :href="til.url">{{ til.title }}</a>
@@ -33,4 +41,25 @@ const categories = computed<{ [key: string]: { title: string; url: string }[] }>
   </section>
 </template>
 
-<style scoped></style>
+<style scoped>
+section {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  grid-gap: 30px;
+  min-width: 70%;
+  margin-bottom: 50px;
+}
+
+h2 {
+  display: grid;
+  grid-template-columns: 40px 1fr;
+  grid-gap: 10px;
+  align-items: center;
+  height: 3rem;
+}
+
+.icon {
+  width: 100%;
+  height: 100%;
+}
+</style>
